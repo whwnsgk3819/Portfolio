28daece333f624fa63a149d21c2980d3458785e7
junha cho <whwnsgk3819@naver.com>
2020-03-24 18:17:11 +0900

    portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,51 +1,60 @@
+$(window).scroll(function () {
+     var scrollTop = $(this).scrollTop();
+     var windowHeight = $(this).height();
+     var scrollBottom = scrollTop + windowHeight;
 
-
-$(window).scroll(function(){
-  var scrollTop = $(this).scrollTop();
-  var windowHeight = $(this).height();
-  var scrollBottom = scrollTop + windowHeight;
-
-     if(scrollTop >= 700){
-     $('.right__arrow').addClass("on");
-     }else{
+     if (scrollTop >= 700) {
+          $('.right__arrow').addClass("on");
+     } else {
           $('.right__arrow').removeClass("on");
-     } 
+     }
 
-     if(scrollBottom >= $('.about').offset().top){
+     if (scrollBottom >= $('.about').offset().top) {
           $('.about').addClass("on");
-     }else{
+     } else {
           $('.about').removeClass("on");
      }
-     if(scrollBottom >= $('.introduce').offset().top){
+     if (scrollBottom >= $('.introduce').offset().top) {
           $('.introduce').addClass("on");
-     }else{
+     } else {
           $('.introduce').removeClass("on");
      }
-     if(scrollBottom >= $('.pf__own').offset().top){
+     if (scrollBottom >= $('.pf__own').offset().top) {
           $('.pf__own').addClass("on");
-     }else{
+     } else {
           $('.pf__own').removeClass("on");
      }
-     if(scrollBottom >= $('.pf__two').offset().top){
+     if (scrollBottom >= $('.pf__two').offset().top) {
           $('.pf__two').addClass("on");
-     }else{
+     } else {
           $('.pf__two').removeClass("on");
      }
-     if(scrollBottom >= $('.pf__three').offset().top){
+     if (scrollBottom >= $('.pf__three').offset().top) {
           $('.pf__three').addClass("on");
-     }else{
+     } else {
           $('.pf__three').removeClass("on");
      }
-     if(scrollBottom >= $('.pf__four').offset().top){
+     if (scrollBottom >= $('.pf__four').offset().top) {
           $('.pf__four').addClass("on");
-     }else{
+     } else {
           $('.pf__four').removeClass("on");
      }
-     if(scrollBottom >= $('.pf__five').offset().top){
+     if (scrollBottom >= $('.pf__five').offset().top) {
           $('.pf__five').addClass("on");
-     }else{
+     } else {
           $('.pf__five').removeClass("on");
      }
+     if (scrollBottom >= $('.pf__five').offset().top) {
+          $('.pf__five').addClass("on");
+     } else {
+          $('.pf__five').removeClass("on");
+     }
+     if (scrollBottom >= $('.pf__six').offset().top) {
+          $('.pf__six').addClass("on");
+     } else {
+          $('.pf__six').removeClass("on");
+     }
+
 
      // if(scrollTop >= 300){
      //  $('.about').addClass("on");
@@ -58,7 +67,7 @@ $(window).scroll(function(){
      // }else{
      //      $('.pf__own').removeClass("on");
      // }
-     
+
      // if(scrollTop >= 2800){
      //      $('.pf__two').addClass('on');
      // }else{
@@ -86,45 +95,43 @@ $(window).scroll(function(){
      // }
 })
 
-$(".right__arrow").click(function(){
+$(".right__arrow").click(function () {
      $('html, body').animate({
-          scrollTop : 0
+          scrollTop: 0
      }, 500);
      return false;
 });
 
-$("#listHome,#buttonHome").click(function(){
+$("#listHome,#buttonHome").click(function () {
      $('html, body').animate({
-          scrollTop : 0
+          scrollTop: 0
      }, 500);
      return false;
 });
-$("#listAbout,#buttonAbout").click(function(){
+$("#listAbout,#buttonAbout").click(function () {
      $('html, body').animate({
-          scrollTop : $('.about').offset().top - 50
+          scrollTop: $('.about').offset().top - 50
      }, 500);
      return false;
 });
-$("#listSkill,#buttonSkill").click(function(){
+$("#listSkill,#buttonSkill").click(function () {
      $('html, body').animate({
-          scrollTop : $('.introduce').offset().top - 180
+          scrollTop: $('.introduce').offset().top - 180
      }, 500);
      return false;
 });
-$("#listPortfolio,#buttonPF").click(function(){
+$("#listPortfolio,#buttonPF").click(function () {
 
      $('html, body').animate({
-          scrollTop : $('.portfolio').offset().top  - 80
+          scrollTop: $('.portfolio').offset().top - 80
      }, 500);
      return false;
 });
 
-var $left =  $(".header__menu-list-button")
-var $button =  $(".button")
+var $left = $(".header__menu-list-button")
+var $button = $(".button")
 
 
-    $left.click(function(){
+$left.click(function () {
      $button.toggleClass("on");
-    })
-   
-        
+})
